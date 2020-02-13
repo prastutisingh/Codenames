e580eca80adc366e9d644c9ed9f0caf9940315f8
Prastuti Singh <singhprastuti@gmail.com>
2020-02-12 22:44:10 -0800

    working version of collect_data

--- collect_data.py
+++ collect_data.py
@@ -1,42 +1,81 @@
 import tkinter as tk
 from functools import partial
-
-answers = open('data_1000_vocab.txt', 'w')
-
-def btn_click(item):
-    answers.write(item.strip() + '\n')
+import sys
 
 def main():
     window = tk.Tk()
     window.title('Codenames Data Collector')
     window.geometry("1200x320")
 
-    file = open('examples_1000_vocab.txt', 'r')
+    def close():
+        window.destroy()
+        sys.exit(0)
 
-    for line in file:
-        full_list = line.strip('\n').split(',')
-        good = full_list[:3]
-        bad = full_list[3:9]
-        clues = full_list[9:]
+    window.protocol("WM_DELETE_WINDOW", close)
 
-        top_frame = tk.Frame(window).pack()
-        bottom_frame = tk.Frame(window).pack(side='bottom')
+    var = tk.StringVar()
 
-        tk.Label(top_frame, text='Good words:', font=('Helvetica', 16)).pack(pady=25)
-        tk.Label(top_frame, text=', '.join(good), fg = 'green', font=('Helvetica', 16)).pack()
-        tk.Label(top_frame, text='Bad words:', font=('Helvetica', 16)).pack(pady=25)
-        tk.Label(top_frame, text=', '.join(bad), fg = 'red', font=('Helvetica', 16)).pack()
+    try:
+        current_count = len(open('data_1000_vocab.txt').readlines())
+    except FileNotFoundError:
+        current_count = 0
+        print('Thanks for doing this!')
 
-        clue_buttons = []
-        for item in clues:
-            clue_buttons.append(tk.Button(bottom_frame, text=item, fg='blue', font=('Helvetica', 16),
-                                          padx=20, command=partial(btn_click, item))
-                                .pack(side='left', padx=20))
+    with open('data_1000_vocab.txt', 'a+') as answers:
+        with open('examples_1000_vocab.txt', 'r') as examples:
+            should_close = tk.BooleanVar()
 
-    window.mainloop()
+            # write chosen clue to answers file when button is clicked
+            def btn_click(i, item):
+                answers.write(str(i - 1) + ' ')
+                answers.write(item.strip() + '\n')
+                var.set(item.strip())
 
-    file.close()
-    answers.close()
+            def close_click():
+                should_close.set(True)
+                var.set("")
+
+            i = 1
+            for line in examples:
+                if i <= current_count:
+                    i += 1
+                    pass
+                else:
+                    full_list = line.strip('\n').split(',')
+                    good = full_list[:3]
+                    bad = full_list[3:9]
+                    clues = full_list[9:]
+
+                    top_frame = tk.Frame(window)
+                    top_frame.pack()
+                    bottom_frame = tk.Frame(window)
+                    bottom_frame.pack(side='bottom')
+
+                    tk.Button(top_frame, text='QUIT', fg='red', font=('Helvetica Bold', 16), padx=10, pady=10,
+                              command=close_click).pack(side='left', padx=10, pady=25)
+
+                    tk.Label(top_frame, text='Good words:', font=('Helvetica', 16)).pack(pady=25)
+                    tk.Label(top_frame, text=', '.join(good), fg='green', font=('Helvetica', 16)).pack()
+                    tk.Label(top_frame, text='Bad words:', font=('Helvetica', 16)).pack(pady=25)
+                    tk.Label(top_frame, text=', '.join(bad), fg='red', font=('Helvetica', 16)).pack()
+
+                    clue_buttons = []
+                    for item in clues:
+                        clue_buttons.append(tk.Button(bottom_frame, text=item, fg='blue', font=('Helvetica', 16),
+                                                      padx=20, pady=10, command=partial(btn_click, i, item)))
+                        clue_buttons[-1].pack(side='left', padx=20, pady=20)
+
+                    # wait for click
+                    window.wait_variable(var)
+
+                    if should_close.get():
+                        break
+
+                    # clear things out
+                    top_frame.destroy()
+                    bottom_frame.destroy()
+
+                    i += 1
 
 if __name__ == '__main__':
     main()
