a842c08f9ddc5eba6f9910f304751a39e501d8c2
Prastuti Singh <singhprastuti@gmail.com>
2020-03-08 16:37:01 -0700

    nnet python notebook

--- data_collector.py
+++ data_collector.py
@@ -5,7 +5,7 @@ import sys
 def main():
     window = tk.Tk()
     window.title('Codenames Data Collector')
-    window.geometry("1600x300")
+    window.geometry("1200x300")
 
     def close():
         window.destroy()
@@ -75,25 +75,27 @@ def main():
                     # Entry box
                     e = tk.Entry(top_frame, textvariable=entry_var)
                     e.grid(row=3, column=4, padx=5, pady=20)
-                    tk.Button(top_frame, text='Enter', fg='DodgerBlue3', font=('Helvetica Bold', 16), relief='raised',
+                    tk.Button(top_frame, text='ENTER', fg='DodgerBlue3', font=('Helvetica Bold', 16), relief='raised',
                               padx=10, pady=2, command=partial(entry, num, entry_var)).grid(row=3, column=5)
 
                     # Clue buttons
                     clue_buttons = []
                     for i, item in enumerate(clues[:5]):
-                        clue_buttons.append(tk.Button(bottom_frame, text=item, fg='blue4', font=('Helvetica', 16),
+                        clue_buttons.append(tk.Button(bottom_frame, text=item, fg='blue4', width=20,
+                                                      font=('Helvetica', 16),
                                                       padx=20, pady=8, command=partial(btn_click, num, item)))
                         clue_buttons[-1].grid(row=1, column=i)
 
                     clue_buttons_2 = []
                     for i, item in enumerate(clues[5:10]):
-                        clue_buttons_2.append(tk.Button(bottom_frame, text=item, fg='blue4', font=('Helvetica', 16),
+                        clue_buttons_2.append(tk.Button(bottom_frame, text=item, fg='blue4', width=20,
+                                                        font=('Helvetica', 16),
                                                         padx=20, pady=8, command=partial(btn_click, num, item)))
                         clue_buttons_2[-1].grid(row=2, column=i)
 
                     # Pass button
                     tk.Button(bottom_frame, text='PASS', fg='DodgerBlue3', font=('Helvetica Bold', 16), relief='raised',
-                              padx=10, pady=5, command=partial(pass_click, num)).grid(row=4, column=4, columnspan=2)
+                              padx=10, pady=5, command=partial(pass_click, num)).grid(row=4, column=1, columnspan=3)
 
                     # wait for click
                     window.wait_variable(var)
